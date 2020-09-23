1af48140ad120d5eec77d28fe78c49207958f584
Vinicius Costa <viniciusbragacosta1998@gmail.com>
2020-09-23 18:25:08 -0300

    att teste

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -6,7 +6,8 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste");
+            Console.ReadKey();
         }
     }
 }
